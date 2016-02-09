031ba5b946aaa0ea944b3e5b8756c172d056dc9b
Kaldoran <kaldoran@live.fr>
2016-02-09 13:39:28 -0500

    prepare for multithread

---
+++ INC/thread.h
@@ -0,0 +1,7 @@
+#ifndef THREAD
+#define THREAD
+
+#include "thread_struct.h"
+
+
+#endif
---
+++ INC/thread_struct.h
@@ -0,0 +1,8 @@
+#ifndef THREAD_STRUCT
+#define THREAD_STRUCT
+
+typedef struct threadParam {
+	int min;
+	int max;
+} threadParam;
+#endif
